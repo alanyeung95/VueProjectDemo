40c6d4fcdcf38ea55cd59b17cfab485bfe98074b
Alan Yeung <alanyeung1995@gmail.com>
2020-09-13 16:00:52 +0800

    chore: Hard code the jwt for now

--- src/components/services/item.ts
+++ src/components/services/item.ts
@@ -1,20 +1,30 @@
-import config from "@/config";
-import httpClient from "@/utils/http/httpClient";
+import config from '@/config'
+import httpClient from '@/utils/http/httpClient'
 
 export function getDownloadItemUrl(id: string): string {
-  return `${config.apiRootUrl}/items/${id}/raw`;
+  return `${config.apiRootUrl}/items/${id}/raw`
 }
 
 export interface CreateItemParams {
-  name: string;
-  description: string;
+  name: string
+  description: string
+}
+
+const requestConfig = {
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5NTk5Nzk0MDIsImlhdCI6MTU5OTk4MzAwMn0.t1gdXbZCwg7Jc-DtXuCQk40V_fPx9rO4bD-LOO-oQKU`
+  }
 }
 
 export async function createItem(params: CreateItemParams): Promise<string> {
-  const { data } = await httpClient.post(`${config.apiRootUrl}/items/`, {
-    name: params.name,
-    description: params.description
-  });
+  const { data } = await httpClient.post(
+    `${config.apiRootUrl}/items/`,
+    {
+      name: params.name,
+      description: params.description
+    },
+    requestConfig
+  )
 
-  return data;
+  return data
 }
