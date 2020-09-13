ac13c154e66941139cc0a45b33a741971fcbc5d6
Alan Yeung <alanyeung1995@gmail.com>
2020-09-13 16:13:06 +0800

    refactor: Remove hard-code value in creating item

--- src/components/CreateItem/CreateItem.vue
+++ src/components/CreateItem/CreateItem.vue
@@ -1,28 +1,35 @@
 <template>
   <v-form ref="form" lazy-validation>
-    <v-text-field label="Name" required></v-text-field>
+    <v-text-field v-model="inputName" label="Name" required></v-text-field>
 
-    <v-text-field label="Description" required></v-text-field>
+    <v-text-field
+      v-model="description"
+      label="Description"
+      required
+    ></v-text-field>
 
     <v-btn color="success" class="mr-4" @click="handleSubmit">Submit</v-btn>
   </v-form>
 </template>
 
 <script lang="ts">
-import { Component, Prop } from "vue-property-decorator";
-import Vue from "vue";
-import Vuetify from "vuetify";
+import { Component, Prop } from 'vue-property-decorator'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 
-import { CreateItemParams, createItem } from "@/components/services/item";
+import { CreateItemParams, createItem } from '@/components/services/item'
 
 @Component({})
 export default class CreateItem extends Vue {
+  public inputName: string = ''
+  public description: string = ''
+
   async handleSubmit() {
     const response = await createItem({
-      name: "123",
-      description: "hello world!!!!"
-    });
-    console.log(response);
+      name: this.inputName,
+      description: this.description
+    })
+    console.log(response)
   }
 }
 </script>
